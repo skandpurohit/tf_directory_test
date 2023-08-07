1c5e974ba58d1ab25052b074bf28b55332572130
Skand Purohit <skandpurohit@gmail.com>
2023-08-06 19:39:23 -0600

    first commit

---
+++ modules/app1/main.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "app1" {
+  ami           = "ami-00970f57473724c10"
+  instance_type = "t3.micro"
+
+  #vpc_security_group_ids = ["aws_security_group.sg_app1.id"]
+  
+  subnet_id = "subnet-0a9eef239e622fb83"
+
+  tags = {
+    Name = "App1"
+  }
+}
+
+# resource "aws_security_group" "sg_app1" {
+#   vpc_id = "vpc-013665efd564c85f2"
+
+#   name        = "allow_app1"
+#   description = "Allow TLS inbound traffic"
+
+#   ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks = ["98.38.106.144/32"]
+#     ipv6_cidr_blocks = []
+#   }
+
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   tags = {
+#     Name = "app1"
+#   }
+# }
